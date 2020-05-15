4147a28240ca551192a97dc3300f3525bd958352
MarcGuillen <marc.guillen.96@gmail.com>
2020-05-15 18:17:44 +0200

    map

---
+++ Assets/ObjectTargetBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectTargetBehaviour : MonoBehaviour
+{
+
+    public GameObject target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SnapToTarget();
+    }
+
+    void SnapToTarget()
+    {
+        transform.position = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
+    }
+}
