cc39308158f5c4584bd29c0157b614286edcb490
AxelAlavedra <axel.alavedra@gmail.com>
2020-05-15 18:29:46 +0200

    working on piece spawner

--- Assets/Piece.cs
+++ Assets/Piece.cs
@@ -1,29 +1,42 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Vuforia;
 
 public class Piece : MonoBehaviour
 {
-    public float max_y, min_y;
-    ImageTargetBehaviour target;
-    GameObject map;
+    public float min_y, max_y;
+    bool dragging = false;
+    Vector3 last_mouse_pos = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
-        map = GameObject.Find("Map");
-        target = transform.GetComponentInParent<ImageTargetBehaviour>();
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(dragging)
+        {
+            Vector3 curren_mouse_pos = Input.mousePosition;
+            if(curren_mouse_pos.y != last_mouse_pos.y)
+            {
+                Vector3 tmp_position = transform.position;
+                float new_y = tmp_position.y + ((curren_mouse_pos.y - last_mouse_pos.y) * Time.deltaTime * 2);
+                tmp_position.y = Mathf.Clamp(new_y, min_y, max_y); 
+                transform.position = tmp_position;
+            }
+            last_mouse_pos = curren_mouse_pos;
+        }
     }
-
     private void OnMouseDown()
     {
-        this.gameObject.transform.SetParent(map.transform);
-        Debug.Log(target.TrackableName);
+        dragging = true;
+        last_mouse_pos = Input.mousePosition;
+    }
+    private void OnMouseUp()
+    {
+        dragging = false;
     }
 }
---
+++ Assets/PieceSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class PieceSpawner : MonoBehaviour
+{
+    public float spawn_y_offset;
+    public GameObject piece_to_spawn;
+    GameObject map;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        map = GameObject.Find("Map");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseDown()
+    {
+        SpawnPiece();
+    }
+
+    private void SpawnPiece()
+    {
+        Vector3 spawn_position = transform.position;
+        spawn_position.y = spawn_y_offset;
+
+        GameObject.Instantiate(piece_to_spawn, spawn_position, Quaternion.identity, map.transform);
+    }
+}
